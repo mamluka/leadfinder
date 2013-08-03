63da547e69d1e7714ed64cf5794bcb3846077a8e
David <david.mazvovsky@gmail.com>
2013-08-03 02:40:15 -0700

    update script

--- add-random-field.rb
+++ add-random-field.rb
@@ -4,31 +4,32 @@ require 'digest/md5'
 phones = Array.new
 start_end = Time.new
 
+logger = Logger.new('random-sort-update.log')
+counter = 0
+
 File.open(ARGV[0]).each do |phone|
   phones << phone.delete("\n")
+
+  counter = counter + 1
 
   if phones.length % 10000 == 0
 
     docs = phones
 
-    update_docs = docs.map { |x| {
-        id: Digest::MD5.hexdigest(x),
-        type: 'household',
-        doc: {
-            random_sort: Random.rand(6000000)
-        }
+    update_docs = docs.map { |x|
+      {
+          id: Digest::MD5.hexdigest(x),
+          type: 'household',
+          script: 'ctx._source.random_sort = round(random()*100000000)'
+      }
     }
-    }
+
 
     s = Tire.index 'leads' do
-
       bulk :update, update_docs
-
     end
 
-    File.open('response.json', 'a') { |f| f.write(s.to_json) }
-
-    p "Took #{Time.new - start_end}"
+    logger.info "Took #{Time.new - start_end}, we processed #{counter}"
     start_end = Time.new
 
     phones.clear
