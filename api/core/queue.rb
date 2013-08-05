ef0d632a1f235d23ab657204593aef81b61c2784
David <david.mazvovsky@gmail.com>
2013-08-05 13:27:49 -0700

    move responder list to right

--- api/core/queue.rb
+++ api/core/queue.rb
@@ -1,11 +1,9 @@
 require 'backburner'
 
-logger = Logger.new('jobs.log')
-
 Backburner.configure do |config|
-  config.beanstalk_url = ["beanstalk://127.0.0.1"]
-  config.tube_namespace = "leads-finder"
-  config.on_error = lambda { |e| logger.error e }
+  config.beanstalk_url = %w(beanstalk://127.0.0.1)
+  config.tube_namespace = 'leads-finder'
+  config.on_error = lambda { |e| puts e }
   config.max_job_retries = 3 # default 0 retries
   config.retry_delay = 2 # default 5 seconds
   config.default_priority = 65536
