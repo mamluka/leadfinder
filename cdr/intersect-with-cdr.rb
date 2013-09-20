25b6433d299947aea51cbec19e89f68b96027ed9
David <david.mazvovsky@gmail.com>
2013-09-20 12:14:28 -0700

    fix asgain

--- cdr/intersect-with-cdr.rb
+++ cdr/intersect-with-cdr.rb
@@ -1,39 +1,45 @@
 require 'redis'
 require 'logger'
 
-redis = redis = Redis.new(:host => "localhost", :port => ENV['CDR_REDIS_PORT'].nil? ? 6379 : ENV['CDR_REDIS_PORT'].to_i)
 
-phones_to_get = Array.new
-counter = 0
-start_time = Time.now
+class ProcessCDR
+  def initialize
+    @redis = Redis.new(:host => "localhost", :port => ENV['CDR_REDIS_PORT'].nil? ? 6379 : ENV['CDR_REDIS_PORT'].to_i)
+    @logger = Logger.new 'log-intersect-with-cdr.log'
+  end
 
-steps = 1000000
 
-logger = Logger.new 'log-intersect-with-cdr.log'
+  def process(phones_to_get)
 
-File.open(ARGV[0]).each do |line|
+    start_time = Time.now
 
-  if phones_to_get.length == steps || phones_to_get.length < steps
+    p phones_to_get
 
-    counter = counter + steps
-
-    s = redis.pipelined do
-      phones_to_get.each { |x| redis.get x[:phone] }
+    s = @redis.pipelined do
+      phones_to_get.each { |x| @redis.get x[:phone] }
     end
     ok_phones = Array.new
     s.each_index { |i| ok_phones << phones_to_get[i] if s[i] == '1' }
 
-    logger.info "Took #{Time.now-start_time} for this cycle, total read #{counter}"
+    @logger.info "Took #{Time.now-start_time} for this cycle"
 
     ok_phones.each { |x| $stdout.puts x[:line] }
 
     phones_to_get.clear
-    start_time = Time.now
+  end
+end
+
+steps = 1000000
+phones_to_get = Array.new
+process = ProcessCDR.new
+
+File.open(ARGV[0]).each do |line|
+  line.scan(/\d{10}/).map { |x| {line: line.strip, phone: x} }.each { |x| phones_to_get << x }
+
+  if phones_to_get.length % steps == 0
+    process.process(phones_to_get)
   end
 
-  phone = line.scan(/\d{10}/)[0]
-
-  #$stdout.puts phone
-
-  phones_to_get << {line: line.strip, phone: phone} if not phone.nil?
 end
+
+process.process(phones_to_get)
