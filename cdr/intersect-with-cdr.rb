c80b48ceadc22e8311d9f6b680b9c78c9dcfb178
David <david.mazvovsky@gmail.com>
2013-09-20 09:49:53 -0700

    intersect to support whole lines

--- cdr/intersect-with-cdr.rb
+++ cdr/intersect-with-cdr.rb
@@ -18,18 +18,20 @@ File.open(ARGV[0]).each do |line|
     counter = counter + steps
 
     s = redis.pipelined do
-      phones_to_get.each { |x| redis.get x }
+      phones_to_get.each { |x| redis.get x[:phone] }
     end
     ok_phones = Array.new
     s.each_index { |i| ok_phones << phones_to_get[i] if s[i] == '1' }
 
     logger.info "Took #{Time.now-start_time} for this cycle, total read #{counter}"
 
-    ok_phones.each { |x| $stdout.puts x } 
+    ok_phones.each { |x| $stdout.puts x[:line] }
 
     phones_to_get.clear
     start_time = Time.now
   end
 
-  phones_to_get << line.strip
+  phone = line.scan(/\d{10}/)[0]
+
+  phones_to_get << {line: line.strip, phone: phone} if not phone.nil?
 end
